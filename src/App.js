bbcd992352f991881d7e826e447b2737c52947bd
junheeyeap <junheeyeap@gmail.com>
2021-01-09 01:37:46 +0900

    Todo-App-Clone with React.js

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import TodoTemplate from './components/TodoTemplate';
+import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
+import TodoListItem from './components/TodoListItem';
+
+
+const App = () => {
+  return ( <TodoTemplate>
+    <TodoInsert/>
+    <TodoList />
+    </TodoTemplate>
+  )
+};
+
+export default App;
